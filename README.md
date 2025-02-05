a029b3d21d4a4f624a7239c97ba7788b41bc2f1f
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2025-02-05 02:29:27 +0100

    support rematching documents and using new documents to update match

--- README.md
+++ README.md
@@ -1,19 +1,5 @@
 # Bicameral
 
-## Assumptions and Notes
-1. Documents arrive in full meaining we don't get one part of the document in one request and get other parts in other request
-2. Documents arrive to our system in one of three ways:
-	a. Push => Another service sends the document to us via a REST API endpoint
-    b. Pull => We periodically poll for a new document from a queue SQS or DB
-	c. Hybrid => When a new document is ready, we are notified via an endpoint and then we pull the document from the source
-3. We can achive processing multiple documents at the same time with FastAPI for the push approach. 
-4. For the pull approach we can handle it via multi threading and multi processing
-5. We can store matches in a database like PostgreSQL
-6. To compute match of a document a sample we compute the match score of the document against each sample and the sample with the highest match score > 0 gets the document.
-7. We compute match score by seeing how many values in the document match with values in the sample
-8. If the document has a match score of zero with all samples then it doesn't match any sample
-
-A lot of the design decision will really depend on what's expected of the system I/O wise.
 
 ## Setup
 - Python version 3.11.6
--- main.py
+++ main.py
@@ -1,8 +1,80 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, BackgroundTasks
 from typing import Any
+from collections import defaultdict
+from pydantic.dataclasses import dataclass
+import logging
 
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 app = FastAPI()
 
+@dataclass
+class MatchedDocument:
+	sample_id: str # id of the sample this document matched
+	match_score: int # the match score
+	document: dict[str, Any] # the document
+
+
+samples_store = [
+		{
+			"Id": "001",
+			"Customer Name": "Redline Auto Center",
+			"Customer #": "10322",
+			"Invoice Number": "IK-901122694",
+			"Invoice Amount": "20",
+			"Invoice Date": "2019-12-05",
+			"Comments": ""
+		},
+		{
+			"Id": "002",
+			"Customer Name": "Titan Heavy Industries",
+			"Customer #": "120429",
+			"Invoice Number": "019854",
+			"Invoice Amount": "55350",
+			"Invoice Date": "2024-02-25",
+			"Comments": ""
+		},
+		{
+			"Id": "003",
+			"Customer Name": "Titan Heavy Industries",
+			"Customer #": "120422",
+			"Invoice Number": "024-2031893",
+			"Invoice Amount": "0",
+			"Invoice Date": "2024-05-05",
+			"Comments": ""
+		},
+		{
+			"Id": "004",
+			"Customer Name": "Bella's Bakery",
+			"Customer #": "21031",
+			"Invoice Number": "2024492186",
+			"Invoice Amount": "2022",
+			"Invoice Date": "2022-05-02",
+			"Comments": ""
+		},
+		{
+			"Id": "005",
+			"Customer Name": "Bread Co",
+			"Customer #": "204203",
+			"Invoice Number": "18587422",
+			"Invoice Amount": "0",
+			"Invoice Date": "2024-03-24",
+			"Comments": ""
+		},
+		{
+			"Id": "006",
+			"Customer Name": "Bread Co",
+			"Customer #": "204209",
+			"Invoice Number": "98531002",
+			"Invoice Amount": "0",
+			"Invoice Date": "2024-01-29",
+			"Comments": ""
+		}
+	]
+unmatched_documents_store: list[dict[str, Any]] = [] # holds list of documents that did not match any sample
+matched_documents_store: list[MatchedDocument] = [] # holds list of documents that matched a sample
+match_store = defaultdict(list) # holds a mapping between the sample and it's matching documents
 
 def get_values(data: list[Any]|dict[str, Any]) -> list[Any]:
 	"""
@@ -20,105 +92,114 @@ def get_values(data: list[Any]|dict[str, Any]) -> list[Any]:
 		result.append(data)
 	return result
 
-def compute_match_score(document: dict[str, Any], sample: dict[str, Any]) -> int:
+def compute_match_score(document: dict[str, Any], sample_or_document: dict[str, Any]) -> int:
 	"""
 	Compute the match score of a document against a sample.
 	This is a simple implementation that counts the number of values in the document that are also in the sample.
 	"""
 	match_score = 0
-	sample_values = get_values(sample)
+	sample_values = get_values(sample_or_document)
 	document_values = get_values(document)
 	for value in document_values:
 		if value in sample_values:
 			match_score += 1
 	return match_score
 
-def find_sample_match(document: dict[str, Any], samples: list[dict[str, Any]]) -> dict[str, Any]:
+def match_document(document: dict[str, Any]) -> MatchedDocument|None:
 	"""
-	Find the sample that matches the document
+	Find the sample that matches the document. Check for a match between the document and each sample
+	and then check for a match between the document and previously matched documents. If the document
+	matches a previously matched document, then return the sample that matched the previously matched document.
 	"""
 	max_match_score = 0
-	matching_sample = None
-	for sample in samples:
+	matching_sample_id = None
+	# check document against each sample
+	for sample in samples_store:
 		match_score = compute_match_score(document, sample)
 		if match_score > max_match_score:
 			max_match_score = match_score
-			matching_sample = sample
-	return matching_sample
+			matching_sample_id = sample["Id"]
+	# check document against previously matched documents
+	for matched_document in matched_documents_store:
+		match_score = compute_match_score(document, matched_document.document)
+		if match_score > max_match_score:
+			max_match_score = match_score
+			matching_sample_id = matched_document.sample_id
+	if matching_sample_id is not None:
+		return MatchedDocument(sample_id=matching_sample_id, document=document, match_score=max_match_score)
+	return None
 
-def fetch_sample_descriptions() -> list[dict[str, Any]]:
+def save_matched_document(matched_document: MatchedDocument) -> None:
 	"""
-	Fetch the sample descriptions from the database.
+	Store/Persist matched document
 	"""
-	return [
-		{
-			"Customer Name": "Redline Auto Center",
-			"Customer #": "10322",
-			"Invoice Number": "IK-901122694",
-			"Invoice Amount": "20",
-			"Invoice Date": "2019-12-05",
-			"Comments": ""
-		},
-		{
-			"Customer Name": "Titan Heavy Industries",
-			"Customer #": "120429",
-			"Invoice Number": "019854",
-			"Invoice Amount": "55350",
-			"Invoice Date": "2024-02-25",
-			"Comments": ""
-		},
-		{
-			"Customer Name": "Titan Heavy Industries",
-			"Customer #": "120422",
-			"Invoice Number": "024-2031893",
-			"Invoice Amount": "0",
-			"Invoice Date": "2024-05-05",
-			"Comments": ""
-		},
-		{
-			"Customer Name": "Bella's Bakery",
-			"Customer #": "21031",
-			"Invoice Number": "2024492186",
-			"Invoice Amount": "2022",
-			"Invoice Date": "2022-05-02",
-			"Comments": ""
-		},
-		{
-			"Customer Name": "Bread Co",
-			"Customer #": "204203",
-			"Invoice Number": "18587422",
-			"Invoice Amount": "0",
-			"Invoice Date": "2024-03-24",
-			"Comments": ""
-		},
-		{
-			"Customer Name": "Bread Co",
-			"Customer #": "204209",
-			"Invoice Number": "98531002",
-			"Invoice Amount": "0",
-			"Invoice Date": "2024-01-29",
-			"Comments": ""
-		}
-	]
+	matched_documents_store.append(matched_document)
+	match_store[matched_document.sample_id].append(matched_document.document)
 
-def update_sample_match(document: dict[str, Any], sample: dict[str, Any]):
+def match_unmatched_documents_task(matched_document: MatchedDocument) -> None:
 	"""
-	Update the sample match in the database. This should ideally store in a database but 
-	for the purpose of this example, we will just print the sample that matches the document.
+	This task will check each unmatched document against the provided matched document and match them if they match.
 	"""
-	print(f"Document: {document}")
-	print(f"Matching Sample: {sample}")
+	logger.info("Matching unmatched documents")
+	for unmatched_document in unmatched_documents_store:
+		match_score = compute_match_score(unmatched_document, matched_document.document)
+		if match_score > 0:
+			unmatched_documents_store.remove(unmatched_document)
+			new_matched_document = MatchedDocument(
+				sample_id=matched_document.sample_id,
+				document=unmatched_document,
+				match_score=match_score
+			)
+			save_matched_document(new_matched_document)
+			logger.info("Matched a previously unmatched document")
+			logger.info(new_matched_document)
+	logger.info("Done matching unmatched documents")
 
+def rematch_matched_documents_task(matched_document: MatchedDocument):
+	"""
+	This task will check each matched document against the provided newly matched document and rematch them if they match better.
+	"""
+	logger.info("Matching matched documents")
+	for already_matched_document in matched_documents_store:
+		match_score = compute_match_score(already_matched_document.document, matched_document.document)
+		if match_score > already_matched_document.match_score:
+			logger.info(f"Found better match, old match score {already_matched_document.match_score} and new match score {match_score}")
+			new_match = MatchedDocument(
+				sample_id=matched_document.sample_id,
+				document=already_matched_document.document,
+				match_score=match_score
+			)
+			save_matched_document(new_match)
+			match_store[already_matched_document.sample_id].remove(already_matched_document.document)
+			logger.info(f"Matched document has been rematched. Old sample Id {already_matched_document.sample_id} and New Sample Id {matched_document.sample_id}")
+	logger.info("Done matching matched documents")
 
 @app.post("/document")
-async def process_document(document: dict[str, Any]):
+async def process_document(document: dict[str, Any], background_tasks: BackgroundTasks):
 	"""
-	Process a document and find the sample that matches the document
+	Process a document and find the sample that matches the document.
+	For every newly received document it will try to match the document with a sample. If it's unable 
+	to find a match for the document, it will store the document in the unmatched_documents list. However,
+	when it does match successfully, it uses this new match to attempt matching with previously unmatched documents and
+	also to see if this document matches better with previously matched samples and then updates the match dictionary.
 	"""
-	samples = fetch_sample_descriptions()
-	matching_sample = find_sample_match(document, samples)
-	if matching_sample:
-		update_sample_match(document, matching_sample)
+	matched_document = match_document(document)
+	if matched_document is not None:
+		save_matched_document(matched_document)
+		# check if the newly matched document has any match with documents that couldn't be matched previously and match them
+		background_tasks.add_task(match_unmatched_documents_task, matched_document)
+		# check if already matched documents match better with this new document and rematch them
+		background_tasks.add_task(rematch_matched_documents_task, matched_document)
 	else:
+		unmatched_documents_store.append(document)
 		raise HTTPException(status_code=404, detail="No matching sample found")
 
+		
+
+
+@app.get("/match")
+def get_match():
+	"""
+	Get the match dictionary
+	"""
+	return match_store
